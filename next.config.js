3adb8c210f44596165a6ba89ed0e37a525e3295c
04yutols <96228235+04yutols@users.noreply.github.com>
2024-06-10 08:29:22 +0900

    next.config.js を更新

--- next.config.js
+++ next.config.js
@@ -5,6 +5,25 @@ module.exports = {
   },
     plugins: [
       '@babel/plugin-transform-modules-commonjs',
+      '@typescript-eslint',
     ],
     transpilePackages: [ "antd", "@ant-design", "rc-util", "rc-pagination", "rc-picker", "rc-notification", "rc-tooltip", "rc-tree", "rc-table" ],
+    parserOptions: {
+    parser: require.resolve('@typescript-eslint/parser'),
+    extraFileExtensions: ['.vue'],
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended'
+  ],
+  overrides: [{
+    files: ['*.ts', '*.tsx'],
+    rules: {
+      // The core 'no-unused-vars' rules (in the eslint:recommeded ruleset)
+      // does not work with type definitions
+      'no-unused-vars': 'off',
+    }
+  }]
   }
